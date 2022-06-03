372147b6d37d996146e74016600ec84ade5ac8f7
onikatashi <93861212+onikatashi@users.noreply.github.com>
2022-06-03 20:58:27 +0900

    CHORE : WebConfig allowedOrigins 수정

--- src/main/java/com/fakedevelopers/bidderbidder/config/WebConfig.java
+++ src/main/java/com/fakedevelopers/bidderbidder/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://116.40.180.205:8080", "http://localhost:80")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
