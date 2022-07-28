9c3cf58eb0a468562fe2c61155324244592eb160
joonamin <joonamin44@gmail.com>
2022-07-29 02:43:03 +0900

    chore: 코드 스멜 제거-2

--- src/main/java/com/fakedevelopers/bidderbidder/util/RequestUtil.java
+++ src/main/java/com/fakedevelopers/bidderbidder/util/RequestUtil.java
@@ -1,27 +1,31 @@
 package com.fakedevelopers.bidderbidder.util;
 
+import javax.servlet.http.HttpServletRequest;
 import org.apache.http.HttpHeaders;
 
-import javax.servlet.http.HttpServletRequest;
-
 public class RequestUtil {
-    // Request 메세지를 검증하고, 토큰 값을 가져온다.
-    public static String getAuthorizationToken(String header) {
-        // Authorization: [Bearer <token>]
-        // @Param: [Bearer <token>]
-        if (header == null || !header.startsWith("Bearer ")) {
-            throw new IllegalArgumentException("유효하지 않은 토큰");
-        }
+  // Request 메세지를 검증하고, 토큰 값을 가져온다.
 
-        String[] parts = header.split(" ");
-        if (parts.length != 2) {
-            throw new IllegalArgumentException("유효하지 않은 토큰");
-        }
-        // 토큰 값을 리턴
-        return parts[1];
+  private RequestUtil() {
+    throw new IllegalAccessError("Utility Class는 인스턴스를 생성할 수 없습니다.");
+  }
+
+  public static String getAuthorizationToken(String header) {
+    // Authorization: [Bearer <token>]
+    // @Param: [Bearer <token>]
+    if (header == null || !header.startsWith("Bearer ")) {
+      throw new IllegalArgumentException("유효하지 않은 토큰");
     }
 
-    public static String getAuthorizationToken(HttpServletRequest request) {
-        return getAuthorizationToken(request.getHeader(HttpHeaders.AUTHORIZATION));
+    String[] parts = header.split(" ");
+    if (parts.length != 2) {
+      throw new IllegalArgumentException("유효하지 않은 토큰");
     }
+    // 토큰 값을 리턴
+    return parts[1];
+  }
+
+  public static String getAuthorizationToken(HttpServletRequest request) {
+    return getAuthorizationToken(request.getHeader(HttpHeaders.AUTHORIZATION));
+  }
 }
