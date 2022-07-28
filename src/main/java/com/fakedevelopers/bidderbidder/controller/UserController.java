a10e125f9ef61deb7632c66fce9626658b929a77
joonamin <joonamin44@gmail.com>
2022-07-29 00:51:37 +0900

    feat: firbase token을 이용한 구글 로그인 API 구현

--- src/main/java/com/fakedevelopers/bidderbidder/controller/UserController.java
+++ src/main/java/com/fakedevelopers/bidderbidder/controller/UserController.java
@@ -3,7 +3,6 @@ package com.fakedevelopers.bidderbidder.controller;
 import com.fakedevelopers.bidderbidder.dto.OAuth2UserRegisterDto;
 import com.fakedevelopers.bidderbidder.dto.UserLoginDto;
 import com.fakedevelopers.bidderbidder.dto.UserRegisterDto;
-import com.fakedevelopers.bidderbidder.message.request.RegisterInfo;
 import com.fakedevelopers.bidderbidder.message.response.UserInfo;
 import com.fakedevelopers.bidderbidder.model.UserEntity;
 import com.fakedevelopers.bidderbidder.service.OAuth2UserService;
@@ -47,26 +46,31 @@ public class UserController {
 
 
   @PostMapping("/signin-google")
-  UserInfo oAuth2GoogleLoginOrRegister(@RequestHeader("Authorization") String authorization,
-      @RequestBody RegisterInfo registerInfo) {
+  UserInfo oAuth2GoogleLoginOrRegister(@RequestHeader("Authorization") String authorization) {
     // Authorization: Bearer <token> 형식
     FirebaseToken decodedToken;
+    final String INIT_NICK_NAME = "신규 유저";
+
     try {
       String token = RequestUtil.getAuthorizationToken(authorization);
       decodedToken = firebaseAuth.verifyIdToken(token);
     } catch (FirebaseAuthException | IllegalArgumentException e) {
-      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰");
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰" + e.getMessage());
     }
 
     // token을 이용하여 가져온 사용자 정보를 통하여, 회원가입 및 로그인 절차를 진행한다.
     UserEntity userEntity;
     try {
       userEntity = (UserEntity) oAuth2UserService.loadUserByUsername(decodedToken.getEmail());
+      if (oAuth2UserService.findByEmail(userEntity.getEmail()) == null) {
+        // 이전에 등록된 적이 없는 경우, 등록 과정 수행
+        userEntity = oAuth2UserService.register(
+            new OAuth2UserRegisterDto(decodedToken.getEmail(), INIT_NICK_NAME));
+      } else {
+        userEntity = oAuth2UserService.findByEmail(userEntity.getEmail());
+      }
     } catch (UsernameNotFoundException e) {
-      // 이전에 등록된 적이 없는 경우, 등록 과정 수행
-      userEntity = oAuth2UserService.register(
-          new OAuth2UserRegisterDto(decodedToken.getEmail(), registerInfo.getNickname())
-      );
+      throw new UsernameNotFoundException("UsernameNotFoundException" + e.getMessage());
     }
     return new UserInfo(userEntity.getEmail(), userEntity.getNickname());
   }
