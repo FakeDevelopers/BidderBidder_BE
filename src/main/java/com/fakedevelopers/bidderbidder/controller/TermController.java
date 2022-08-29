9b69471f09241b46fa9aff7728c6b72102074f66
konempty <jameswebb@naver.com>
2022-08-29 21:14:24 +0900

    BDBD-436 필수 약관과 선택약관 분리

--- src/main/java/com/fakedevelopers/bidderbidder/controller/TermController.java
+++ src/main/java/com/fakedevelopers/bidderbidder/controller/TermController.java
@@ -1,13 +1,14 @@
 package com.fakedevelopers.bidderbidder.controller;
 
 import com.fakedevelopers.bidderbidder.service.TermService;
-import java.util.List;
+import com.google.gson.JsonObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,7 +24,7 @@ public class TermController {
 	}
 
 	@GetMapping("/list/")
-	List<String> getTerms() {
+	JsonObject getTerms() {
 		return termService.getTerms();
 	}
 
@@ -33,9 +34,10 @@ public class TermController {
 	}
 
 	@PostMapping(value = "/{termName}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-	String addTerm(@PathVariable String termName, @RequestPart MultipartFile term)
+	String addTerm(@PathVariable String termName, @RequestPart MultipartFile term,
+			@RequestParam Boolean isRequired)
 			throws Exception { // 나중에 요 API는 관리자만? 접근하도록 해야 할것 같네요 ㅋㅋㅋ
-		termService.addTerm(termName, term);
+		termService.addTerm(termName, isRequired, term);
 		return "success";
 	}
 
--- src/main/java/com/fakedevelopers/bidderbidder/service/TermService.java
+++ src/main/java/com/fakedevelopers/bidderbidder/service/TermService.java
@@ -1,6 +1,8 @@
 package com.fakedevelopers.bidderbidder.service;
 
 import com.fakedevelopers.bidderbidder.exception.HttpException;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,9 +11,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
@@ -21,50 +21,75 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class TermService {
 
-	private static final String TERM_FOLDER = "./terms";
+	private static final String TERM_FOLDER = "./terms" + File.separator;
 	private static final String TERM_EXTENSION = ".md";
 	private static final int TERM_EXTENSION_LENGTH = TERM_EXTENSION.length();
+	private static final String REQUIRED = "required";
+	private static final String OPTIONAL = "optional";
+	private static final String[] TERM_TYPES = {REQUIRED, OPTIONAL};
 
-	public List<String> getTerms() {
-		String path = TERM_FOLDER + File.separator;
-		File directory = new File(path);
-		return Arrays.stream(Objects.requireNonNull(directory.list()))
-				.map(it -> it.substring(0, it.length() - TERM_EXTENSION_LENGTH)).collect(
-						Collectors.toList());
+	public JsonObject getTerms() {
+		JsonObject jsonObject = new JsonObject();
+
+		for (String termType : TERM_TYPES) {
+			JsonArray termList = new JsonArray();
+			Arrays.stream(Objects.requireNonNull(
+							new File(TERM_FOLDER + termType).list()))
+					.forEach(
+							it -> termList.add(it.substring(0, it.length() - TERM_EXTENSION_LENGTH)));
+
+			jsonObject.add(termType, termList);
+		}
+		return jsonObject;
 	}
 
 	@Cacheable(value = "terms", key = "#termName")
 	public String getTerm(String termName) {
-		String filePath = TERM_FOLDER + File.separator + termName + TERM_EXTENSION;
-		File term = new File(filePath);
-		if (term.exists()) {
-			try (InputStream inputStream = new FileInputStream(term)) {
-				return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-			} catch (Exception e) {
-				throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+
+		for (String termType : TERM_TYPES) {
+			File termFile = getTermFile(termType, termName);
+			if (termFile.exists()) {
+				try (InputStream inputStream = new FileInputStream(termFile)) {
+					return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+				} catch (Exception e) {
+					throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+				}
 			}
 		}
+
 		throw new HttpException(HttpStatus.NOT_FOUND, "존재하지 않는 약관입니다.");
 	}
 
 	@CacheEvict(value = "terms", key = "#termName") // 약관이 업데이트 되면 해당 약관의 캐싱을 삭제합니다.
-	public void addTerm(String termName, MultipartFile file) throws Exception {
-		String filePath = TERM_FOLDER + File.separator + termName + TERM_EXTENSION;
-		File term = new File(filePath);
+	public void addTerm(String termName, Boolean isRequired, MultipartFile file) throws Exception {
+		File saveFile = getTermFile((isRequired ? REQUIRED : OPTIONAL), termName);
+		File deleteFile = getTermFile((isRequired ? OPTIONAL : REQUIRED), termName);
 
-		Files.createDirectories(Path.of(TERM_FOLDER));
-		try (FileOutputStream outputStream = new FileOutputStream(term)) {
+		Files.createDirectories(Path.of(TERM_FOLDER + REQUIRED));
+		Files.createDirectories(Path.of(TERM_FOLDER + OPTIONAL));
+		try (FileOutputStream outputStream = new FileOutputStream(saveFile)) {
 			outputStream.write(file.getBytes());
+		}
+		if (deleteFile.exists() && !deleteFile.delete()) {
+			throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, "파일삭제에 실패하였습니다.");
 		}
 	}
 
 	@CacheEvict(value = "terms", key = "#termName")
 	public boolean deleteTerm(String termName) {
-		String filePath = TERM_FOLDER + File.separator + termName + TERM_EXTENSION;
-		File term = new File(filePath);
-		if (term.exists()) {
-			return term.delete();
+
+		for (String termType : TERM_TYPES) {
+			File termFile = getTermFile(termType, termName);
+			if (termFile.exists() && termFile.delete()) {
+				return true;
+			}
 		}
 		return false;
+	}
+
+	private File getTermFile(String termType, String termName) {
+		String termFilePath =
+				TERM_FOLDER + termType + File.separator + termName + TERM_EXTENSION;
+		return new File(termFilePath);
 	}
 }
