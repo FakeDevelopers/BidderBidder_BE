84c7a15ce8f852b2077461584a5ad411d4fb376c
joonamin <joonamin44@gmail.com>
2022-09-15 00:27:43 +0900

    카카오 로그인 - 액세스 토큰 발급

---
+++ src/main/java/com/fakedevelopers/bidderbidder/domain/OAuthProfile.java
@@ -0,0 +1,14 @@
+package com.fakedevelopers.bidderbidder.domain;
+
+public class OAuthProfile {
+
+  public OAuthProfile() {
+    throw new IllegalStateException("Utility Class");
+  }
+
+  public static final String KAKAO_PREFIX = "kakao_";
+  public static final String GOOGLE_PREFIX = "google_";
+  public static final String NAVER_PREFIX = "naver_";
+  public static final String APPLE_PREFIX = "apple_";
+
+}
--- src/main/java/com/fakedevelopers/bidderbidder/dto/OAuth2UserRegisterDto.java
+++ src/main/java/com/fakedevelopers/bidderbidder/dto/OAuth2UserRegisterDto.java
@@ -1,7 +1,9 @@
 package com.fakedevelopers.bidderbidder.dto;
 
+import com.fakedevelopers.bidderbidder.model.UserEntity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,10 @@ import org.hibernate.validator.constraints.Length;
 @RequiredArgsConstructor
 public class OAuth2UserRegisterDto {
 
+  @NotBlank(message = "사용자ID는 공백문자가 포함될 수 없습니다")
+  @Pattern(message = "사용자ID에는 영문자, 숫자, _만 포함 가능합니다", regexp = "[a-zA-Z0-9_]")
+  private final String username;
+
   @Email(message = "이메일의 형식을 따라야 합니다.")
   @NotBlank(message = "이메일에는 공백문자가 포함될 수 없습니다")
   private final String email;
@@ -24,4 +30,12 @@ public class OAuth2UserRegisterDto {
   @NotBlank(message = "형식이 잘못되었습니다.")
   @Length(min = 3, max = 12)
   private final String nickname;
+
+  public UserEntity toUserEntity() {
+    return UserEntity.builder()
+        .username(username)
+        .email(email)
+        .nickname(nickname)
+        .build();
+  }
 }
