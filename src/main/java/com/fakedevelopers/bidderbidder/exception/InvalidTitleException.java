5972875cb950bbac6ce461ef0fd5107e9f2479cb
onikatashi <93861212+onikatashi@users.noreply.github.com>
2022-09-22 18:19:34 +0900

    CHORE : 작업파일 코드 스타일 수정

--- src/main/java/com/fakedevelopers/bidderbidder/exception/InvalidTitleException.java
+++ src/main/java/com/fakedevelopers/bidderbidder/exception/InvalidTitleException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public class InvalidTitleException extends HttpException {
 
-    public InvalidTitleException(String message) {
-        super(HttpStatus.BAD_REQUEST, message);
-    }
+  public InvalidTitleException(String message) {
+    super(HttpStatus.BAD_REQUEST, message);
+  }
 
 }
