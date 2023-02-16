2efbb71c088bc5afc2acda4342b840e96c1b8fdd
joonamin <joonamin44@gmail.com>
2023-02-16 15:25:20 +0900

    feat(`UserService`): 네이티브 회원가입 구현
    firebase auth를 이용한 회원가입

--- src/main/java/com/fakedevelopers/bidderbidder/model/UserEntity.java
+++ src/main/java/com/fakedevelopers/bidderbidder/model/UserEntity.java
@@ -1,23 +1,18 @@
 package com.fakedevelopers.bidderbidder.model;
 
-import static com.fakedevelopers.bidderbidder.domain.Constants.MAX_USERNAME_SIZE;
-import static com.fakedevelopers.bidderbidder.domain.Constants.MIN_USERNAME_SIZE;
-
-import java.util.Collection;
-import java.util.Collections;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fakedevelopers.bidderbidder.dto.UserRegisterDto;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Collections;
+
+import static com.fakedevelopers.bidderbidder.domain.Constants.*;
 
 /**
  * The type User entity.
@@ -25,81 +20,94 @@ import org.springframework.security.core.userdetails.UserDetails;
  * 패스워드가 null 일 경우, OAuth2 로그인 방식으로 접속한 것. <br> 내부 회원가입은 반드시 not-null 이어야한다.
  * </p>
  */
-@NoArgsConstructor
-@Data
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
 @Entity
 public class UserEntity implements UserDetails {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-  @Column(unique = true, nullable = false)
-  @Length(min = MIN_USERNAME_SIZE, max = MAX_USERNAME_SIZE)
-  private String username; // 유저를 고유하게 구분할 수 있는 String을 의미합니다.
+    @Column(unique = true, nullable = false)
+    @Length(min = MIN_USERNAME_SIZE, max = MAX_USERNAME_SIZE)
+    private String username; // 유저를 고유하게 구분할 수 있는 String을 의미합니다.
 
-  @Email(message = "이메일의 형식을 따라야 합니다.")
-  @Column(unique = true)
-  private String email;
+    @Email(message = "이메일의 형식을 따라야 합니다.")
+    @Column(unique = true)
+    private String email;
 
-  @Column(nullable = false)
-  @NotBlank(message = "형식이 잘못되었습니다.")
-  @Length(min = 3, max = 12)
-  private String nickname;
+    @Column(nullable = false)
+    @NotBlank(message = "형식이 잘못되었습니다.")
+    @Length(min = 3, max = 12)
+    private String nickname;
 
-  @Column
-  private String password;
+    @Column
+    @Length(min = MIN_PASSWORD_SIZE, max = MAX_PASSWORD_SIZE)
+    private String password;
 
-  /**
-   * Instantiates a new User entity. *
-   *
-   * @param username 아이디는 6~12글자의 (영문자, 숫자, _)만 사용이 가능
-   * @param email    이메일 형식 준수, nullable
-   * @param nickname 기본값(Constants.java) 참고
-   * @param password null일 경우 OAuth 로그인
-   */
-  @Builder
-  public UserEntity(String username, String email, String nickname, String password) {
-    this.username = username;
-    this.email = email;
-    this.nickname = nickname;
-    this.password = password;
-  }
+    /**
+     * Instantiates a new User entity. *
+     *
+     * @param username 아이디는 6~12글자의 (영문자, 숫자, _)만 사용이 가능
+     * @param email    이메일 형식 준수, nullable
+     * @param nickname 기본값(Constants.java) 참고
+     * @param password null일 경우 OAuth 로그인
+     */
+    @Builder
+    public UserEntity(Long id, String username, String email, String nickname, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+    }
 
-  /* 아래는 firebase와 관련된 내용 */
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return Collections.emptyList();
-  }
+    public static UserEntity of(UserRegisterDto dto) {
+        return UserEntity.builder()
+                .username(dto.getUsername())
+                .email(dto.getEmail())
+                .nickname(dto.getNickname())
+                .password(dto.getPassword())
+                .build();
+    }
 
-  @Override
-  public String getPassword() {
-    return null;
-  }
+    /* 아래는 firebase와 관련된 내용 */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public String getUsername() {
-    // User Identifier
-    return this.username;
-  }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return false;
-  }
+    @Override
+    public String getUsername() {
+        // User Identifier
+        return this.username;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return false;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return false;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return false;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- src/test/java/com/fakedevelopers/bidderbidder/service/UserServiceTest.java
+++ src/test/java/com/fakedevelopers/bidderbidder/service/UserServiceTest.java
@@ -1,103 +1,153 @@
 package com.fakedevelopers.bidderbidder.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.fakedevelopers.bidderbidder.IntegrationTestBase;
 import com.fakedevelopers.bidderbidder.domain.Constants;
 import com.fakedevelopers.bidderbidder.dto.OAuth2UserRegisterDto;
+import com.fakedevelopers.bidderbidder.dto.UserRegisterDto;
 import com.fakedevelopers.bidderbidder.model.UserEntity;
 import com.fakedevelopers.bidderbidder.repository.UserRepository;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Transactional
-@DisplayName("UserService 클래스")
+@DisplayName("UserService 클래스의")
 public class UserServiceTest extends IntegrationTestBase {
 
-  @Autowired
-  public UserRepository userRepository;
-  @Autowired
-  public UserService userService;
+    @Autowired
+    public UserRepository userRepository;
+    @Autowired
+    public UserService userService;
 
 
-  public static Stream<Arguments> validRegisterDtoGenerator() {
-    ThreadLocalRandom randomGenerator = ThreadLocalRandom.current();
-    return Stream.of(Arguments.of("validUser" + randomGenerator.nextInt(1, 1000), "a@b.com",
-            Constants.INIT_NICKNAME), // 기본 닉네임
-        Arguments.of("validUser" + randomGenerator.nextInt(1, 1000), "a@b.com",
-            "mockup"));
-  }
-
-  public static Stream<Arguments> invalidRegisterDtoGenerator() {
-    ThreadLocalRandom randomGenerator = ThreadLocalRandom.current();
-    return Stream.of(
-        // username 이 6글자 미만인 경우 - 1 (empty)
-        Arguments.of("", "a@b.com", Constants.INIT_NICKNAME),
-
-        // username이 6글자 미만인 경우 - 2
-        Arguments.of("aaa", "a@b.com", Constants.INIT_NICKNAME),
-
-        // username이 64글자를 초과
-        Arguments.of("invalidUser".repeat(30), "a@b.com", Constants.INIT_NICKNAME),
-
-        // 이메일 형식에 맞지 않는 정보가 들어왔을 때
-        Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "invalidEmail",
-            Constants.INIT_NICKNAME),
-
-        // nickname이 3글자 미만인 경우 - 1 (Empty)
-        Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "a@b.com", ""),
-
-        // nickname이 3글자 미만인 경우 - 2
-        Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "a@b.com", "a"),
-
-        // nickname이 12글자 초과인 경우
-        Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "a@b.com", "a".repeat(20)));
-  }
-
-
-  @Nested
-  @DisplayName("Register 메소드는")
-  class Describe_Register {
-
-    @Nested
-    @DisplayName("정당한 OAuth2RegisterDto에 대해")
-    class Context_Validate_Input {
-
-      @ParameterizedTest
-      @MethodSource(value = "com.fakedevelopers.bidderbidder.service.UserServiceTest#validRegisterDtoGenerator")
-      @DisplayName("올바른 UserEntity를 반환한다")
-      void returns_valid_UserEntity(String username, String email, String nickname) {
-        UserEntity result = userService.register(
-            OAuth2UserRegisterDto.builder().username(username).email(email).nickname(nickname)
-                .build());
-        assertThat(result.getUsername()).isEqualTo(username);
-        assertThat(result.getEmail()).isEqualTo(email);
-        assertThat(result.getNickname()).startsWith(nickname);
-      }
+    public static Stream<Arguments> validRegisterDtoGenerator() {
+        ThreadLocalRandom randomGenerator = ThreadLocalRandom.current();
+        return Stream.of(Arguments.of("validUser" + randomGenerator.nextInt(1, 1000), "a@b.com",
+                        Constants.INIT_NICKNAME), // 기본 닉네임
+                Arguments.of("validUser" + randomGenerator.nextInt(1, 1000), "a@b.com",
+                        "mockup"));
     }
 
-    @Nested
-    @DisplayName("정당하지 못한 OAuth2RegisterDto에 대해")
-    class Context_Invalid_Input {
+    public static Stream<Arguments> invalidRegisterDtoGenerator() {
+        ThreadLocalRandom randomGenerator = ThreadLocalRandom.current();
+        return Stream.of(
+                // username 이 6글자 미만인 경우 - 1 (empty)
+                Arguments.of("", "a@b.com", Constants.INIT_NICKNAME),
 
-      @ParameterizedTest
-      @MethodSource(value = "com.fakedevelopers.bidderbidder.service.UserServiceTest#invalidRegisterDtoGenerator")
-      @DisplayName("저장하지 않는다")
-      void Do_Not_Save(String username, String email, String nickname) {
-        OAuth2UserRegisterDto dto = OAuth2UserRegisterDto.builder().username(username).email(email)
-            .nickname(nickname).build();
+                // username이 6글자 미만인 경우 - 2
+                Arguments.of("aaa", "a@b.com", Constants.INIT_NICKNAME),
 
-        Assertions.assertThrows(Exception.class, () -> userService.register(dto));
-      }
+                // username이 64글자를 초과
+                Arguments.of("invalidUser".repeat(30), "a@b.com", Constants.INIT_NICKNAME),
+
+                // 이메일 형식에 맞지 않는 정보가 들어왔을 때
+                Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "invalidEmail",
+                        Constants.INIT_NICKNAME),
+
+                // nickname이 3글자 미만인 경우 - 1 (Empty)
+                Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "a@b.com", ""),
+
+                // nickname이 3글자 미만인 경우 - 2
+                Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "a@b.com", "a"),
+
+                // nickname이 12글자 초과인 경우
+                Arguments.of("invalidUser" + randomGenerator.nextInt(1, 1000), "a@b.com", "a".repeat(20)));
     }
-  }
+
+
+    @Nested
+    @DisplayName("Register 메소드는")
+    class Describe_Register {
+
+        private final Random random = new Random();
+        private final String randomPassword = random.ints(97, 123)
+                .limit(12)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        @Test
+        void randomPasswordGenerateTest() {
+            assertThat(randomPassword).isNotNull();
+            assertThat(randomPassword.length()).isBetween(Constants.MIN_PASSWORD_SIZE, Constants.MAX_PASSWORD_SIZE);
+            Logger logger = LoggerFactory.getLogger(this.getClass());
+            logger.info("Generated Password -> " + randomPassword);
+        }
+
+
+        @Nested
+        @DisplayName("정당한 신규 회원에 대해")
+        class Context_register_new_member {
+
+            @ParameterizedTest
+            @MethodSource(value = "com.fakedevelopers.bidderbidder.service.UserServiceTest#validRegisterDtoGenerator")
+            @DisplayName("파이어베이스 토큰 값을 반환한다.")
+            void It_returns_firebase_custom_token(String username, String email, String nickname) throws FirebaseAuthException {
+                UserRegisterDto userRegisterDto = UserRegisterDto.builder()
+                        .username(username)
+                        .email(email)
+                        .nickname(nickname)
+                        .password(randomPassword)
+                        .build();
+
+                String firebaseCustomToken = userService.register(userRegisterDto);
+                assertThat(firebaseCustomToken).isInstanceOf(String.class);
+                // 테스트가 성공한다면, firebase에 방금 저장된 테스트 유저정보를 삭제합니다
+                final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                try {
+                    firebaseAuth.deleteUser(userRegisterDto.getUsername());
+                } catch (FirebaseAuthException e) {
+                    throw new RuntimeException("Cannot delete Firebase user! ==> uid: " + userRegisterDto.getUsername());
+                }
+            }
+        }
+
+        @Nested
+        @DisplayName("정당한 OAuth2RegisterDto에 대해")
+        class Context_Validate_Input {
+
+            @ParameterizedTest
+            @MethodSource(value = "com.fakedevelopers.bidderbidder.service.UserServiceTest#validRegisterDtoGenerator")
+            @DisplayName("올바른 UserEntity를 반환한다")
+            void returns_valid_UserEntity(String username, String email, String nickname) {
+                UserEntity result = userService.register(
+                        OAuth2UserRegisterDto.builder().username(username).email(email).nickname(nickname)
+                                .build());
+                assertThat(result.getUsername()).isEqualTo(username);
+                assertThat(result.getEmail()).isEqualTo(email);
+                assertThat(result.getNickname()).startsWith(nickname);
+            }
+        }
+
+        @Nested
+        @DisplayName("정당하지 못한 OAuth2RegisterDto에 대해")
+        class Context_Invalid_Input {
+
+            @ParameterizedTest
+            @MethodSource(value = "com.fakedevelopers.bidderbidder.service.UserServiceTest#invalidRegisterDtoGenerator")
+            @DisplayName("저장하지 않는다")
+            void Do_Not_Save(String username, String email, String nickname) {
+                OAuth2UserRegisterDto dto = OAuth2UserRegisterDto.builder().username(username).email(email)
+                        .nickname(nickname).build();
+
+                Assertions.assertThrows(Exception.class, () -> userService.register(dto));
+            }
+        }
+    }
 
 }
